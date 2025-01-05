d252c952539fc243eb610b404e4ba8afb3445e56
Aswabnth.v <aswanthv695@gmail.com>
2025-01-04 23:30:57 -0800

    added router dom & strat work setion

--- my-react-app/src/components/Hero/Hero.jsx
+++ my-react-app/src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import { TagCloud } from "@frank-mayer/react-tag-cloud";
 
 function Hero({who}) {
   return (
@@ -8,10 +9,30 @@ function Hero({who}) {
       <p className='para'>0.1</p>
       <h1 className='intro-head'>Who I Am</h1>
       <p className='who-iam-para'>
-
         {who}
       </p>
+      <a style={{color:'white'}}>Let's get going, amigo!</a>
       </div>
+      <div className="hero-textsphere">
+                <TagCloud
+                    options={(w) => ({
+                        radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
+                        maxSpeed: "fast",
+                    })}
+                    onClick={(tag) => alert(tag)}
+                    onClickOptions={{ passive: true }}
+                >
+                    {[
+                         "HTML", "CSS", "JavaScript", "React", "Node.js", 
+                         "MongoDB", "Express", "Redux", "Next.js", "TailwindCSS",
+                         "SQL", "TypeScript", "Angular", "Java", "C++", 
+                         "Three.js", "Python", "Django", "GraphQL", "Firebase", 
+                         "AWS", "Docker", "PostgreSQL", "MySQL", "Vue.js", 
+                         "Flutter", "PHP", "Swift", "Kotlin", "Figma", 
+                         "Jenkins", "JIRA", "Bootstrap", "Gatsby"
+                    ]}
+                </TagCloud>
+            </div>
     </div>
   );
 }
